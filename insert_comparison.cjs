dc35dfd35bdda0a24562fe2f21365584f8f4fdcf
conversioao <conversioao@gmail.com>
2026-04-24 01:07:00 +0100

    X

--- insert_comparison.cjs
+++
@@ -1,8 +0,0 @@
-const { Client } = require('pg'); 
-const client = new Client({ connectionString: 'postgres://postgres:GSHCVcBgoA3Q5K4pnsqoU8eo@161.97.77.110:5432/conversioai' }); 
-
-client.connect().then(async () => { 
-  await client.query(`INSERT INTO landing_media (slot_id, media_url, media_type, description) VALUES ('comparison_video', '/videos/comparacao.mp4', 'video', 'Vídeo de Fundo da Secção de Comparação') ON CONFLICT (slot_id) DO NOTHING`); 
-  console.log('Slots inserted'); 
-  client.end(); 
-}).catch(e => console.error(e));
--- update_vibra.cjs
+++
@@ -1,8 +0,0 @@
-const { Client } = require('pg'); 
-const client = new Client({ connectionString: 'postgres://postgres:GSHCVcBgoA3Q5K4pnsqoU8eo@161.97.77.110:5432/conversioai' }); 
-
-client.connect().then(async () => { 
-  await client.query(`UPDATE prompt_agents SET name = 'VIBRA PREMIUM' WHERE technical_id = 'lifestyle-aspiracional-video'`); 
-  console.log('prompt_agents updated'); 
-  client.end(); 
-}).catch(e => console.error(e));
